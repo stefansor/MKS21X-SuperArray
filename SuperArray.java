5d019c85214f4a2739c351871b9fee04a39a16de
stefansor <ssorobay00@stuy.edu>
2018-10-24 00:39:41 -0400

    steps 1-3

---
+++ SuperArray.java
@@ -0,0 +1,73 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+  public SuperArray(){
+    data = new String[10];
+  }
+  public SuperArray(int size){
+    data = new String[size];
+  }
+  public void clear(){
+    this.data = new String[this.data.length];
+    this.size = 0;
+  }
+  public int size(){
+    int count = 0;
+    for(int i = 0; i < this.data.length; i++){
+      if(!this.data[i].equals("null")){
+        count = count + 1;
+      }
+    }
+    return count;
+  }
+  public boolean isEmpty(){
+    if(this.size() == 0){
+      return true;
+    }
+    return false;
+  }
+  public boolean add(String elem){
+    if(this.size() >= 10){
+      this.data = new String[this.data.length + 1];
+      this.data[this.data.length - 1] = elem;
+      return true;
+    }
+    if(this.size() < 10){
+      this.data[this.size()] = elem;
+      return true;
+    }
+    return false;
+  }
+  public String get(int index){
+    if(index < this.size()){
+      return this.data[index];
+    }
+    else{
+      return null;
+    }
+  }
+  public String set(int index, String elem){
+    if(index < this.size()){
+      String old = this.data[index];
+      this.data[index] = elem;
+      return old;
+    }
+    else{
+      return null;
+    }
+  }
+  public String toString(){
+    String[] newp = new String[this.size()];
+    int a = 0;
+    for(int i = 0; i < this.data.length; i++){
+      if(!this.data[i].equals("null")){
+        newp[a] = this.data[i];
+        a = a + 1;
+      }
+    }
+    String format = "" + newp;
+    String newformat = format.replaceAll("\"", "");
+    return newformat;
+  }
+
+}
