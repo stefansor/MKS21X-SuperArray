f871f584018a6948711a63d3e4c94015c5133a1b
stefansor <ssorobay00@stuy.edu>
2018-10-25 21:51:00 -0400

    courtesy of Amanda Chen

--- Driver.java
+++ Driver.java
@@ -44,5 +44,81 @@ public class Driver {
 
 
     /**************************************************************************/
+
+        System.out.println("Checking SA for \"Fortnite\":      " + SA.contains("Fortnite"));
+        System.out.println("This should return true!");
+        System.out.println("Checking SA for \"jedi master\"    " + SA.contains("jedi master"));
+        System.out.println("This should return false!\n\n");
+
+    /**************************************************************************/
+    /**phase 2 testing start*/
+    System.out.println("Phase 2 testing, start!");
+    //testing resize()
+    SuperArray letters = new SuperArray();
+
+    for (int x = 0; x < 20; x++){
+      System.out.println();
+      letters.add("a");
+      System.out.println("Size: " + letters.size());
+      System.out.println(letters);
+    } //array should now be filled to index 19
+
+    System.out.println();
+    System.out.println("Phase 2 testing, end");
+    /** phase 2 testing end*/
+
+    System.out.println();
+    System.out.println();
+
+    /** phase 2 testing end*/
+    System.out.println("Phase 3 testing, start!");
+    System.out.println();
+
+    //testing contains()
+    SuperArray randomLetters = new SuperArray();
+    randomLetters.add("b");
+    randomLetters.add("f");
+    randomLetters.add("g");
+    randomLetters.add("c");
+    randomLetters.add("z");
+
+    System.out.println(randomLetters); //should print [b,f,g,c,z]
+
+    if (randomLetters.contains("a")){
+      System.out.println("a is in the array");
+    } else {
+      System.out.println("a is not in the array");
+    } //should print "a is not in the array"
+
+    if (randomLetters.contains("c")){
+      System.out.println("c is in the array");
+    } else {
+      System.out.println("c is not in the array");
+    } //should print "c is in the array"
+
+    System.out.println();
+
+    //testing indexOf() and lastIndexOf()
+    randomLetters.add("b");
+    randomLetters.add("z");
+    System.out.println(randomLetters); //should print [b,f,g,c,z,b,z]
+
+    System.out.println("The first occurance of b is at: " + randomLetters.indexOf("b")); //should print 0
+    System.out.println("The first occurance of f is at: " + randomLetters.indexOf("f")); //should print 1
+    System.out.println("The first occurance of g is at: " + randomLetters.indexOf("g")); //should print 2
+    System.out.println("The first occurance of c is at: " + randomLetters.indexOf("c")); //should print 3
+    System.out.println("The first occurance of z is at: " + randomLetters.indexOf("z")); //should print 4
+    System.out.println("The first occurance of j is at: " + randomLetters.indexOf("j")); //should print -1
+
+    System.out.println();
+    
+    System.out.println("The last occurance of b is at: " + randomLetters.lastIndexOf("b")); //should print 5
+    System.out.println("The last occurance of f is at: " + randomLetters.lastIndexOf("f")); //should print 1
+    System.out.println("The last occurance of g is at: " + randomLetters.lastIndexOf("g")); //should print 2
+    System.out.println("The last occurance of c is at: " + randomLetters.lastIndexOf("c")); //should print 3
+    System.out.println("The last occurance of z is at: " + randomLetters.lastIndexOf("z")); //should print 6
+    System.out.println("The last occurance of j is at: " + randomLetters.lastIndexOf("j")); //should print -1
+
+    System.out.println();
   }
 }
